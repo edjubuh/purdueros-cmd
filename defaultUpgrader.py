bb387a6605e8651d0e2f250bf446d723440edeb3
Elliot Berman <edjubuh@elliotjb.com>
2015-10-03 21:48:17 -0400

    Flesh out defaultUpgrader, finishing touches on pros.py

--- defaultUpgrader.py
+++ defaultUpgrader.py
@@ -1,7 +1,57 @@
 #!/usr/bin/env python
 
-def upgrade(path):
-	print('Upgraded ' + path)
+# The default upgrader module assumes the following files need to be upgraded:
+#	- firmware/libccos.a
+#	- firmware/uniflash.jar
+#	- include/API.h
+#	- src/Makefile
+#	- Makefile
 
-def create(path):
-	print('Created ' + path)
+# The default upgrader module assumes the following files exist beyond the ones listed above
+#	- .project
+
+import os, errno
+import shutil
+import tempfile
+
+upgradeFiles = ['firmware' + os.path.sep + 'libccos.a', 
+				'firmware' + os.path.sep + 'uniflash.jar', 
+				'include' + os.path.sep + 'API.h',
+				'src' + os.path.sep + 'Makefile',
+				'Makefile' ]
+
+def upgrade(path, kernel, projectName):
+	for foo in upgradeFiles: 
+		print('Upgrading ' + foo)
+		shutil.copyfile(kernel + os.path.sep + foo, path + os.path.sep + foo)
+		
+	print('\nUpgraded project to ' + kernel.split(os.path.sep)[-1])
+
+def create(path, kernel, projectName):
+	if(os.path.exists(path)):
+		print('Directory already exists, removing.')
+		shutil.rmtree(path)
+	
+	copyText = 'Copying from ' + kernel.split(os.path.sep)[-1] + '...'
+	print(copyText),
+	shutil.copytree(kernel, path)
+	copiedText = 'Copied from ' + kernel.split(os.path.sep)[-1] 
+	print('\r' + copiedText + ' ' * (len(copyText) - len(copiedText)))
+	
+	print('Fixing template files')
+	replaceTextInFile(path + os.path.sep + '.project', 'Default_VeX_Cortex', projectName)
+
+
+def replaceTextInFile(path, original, new):
+	tempf = tempfile.NamedTemporaryFile(delete=False)
+	foo_file = open(path, 'r')
+	for line in foo_file:
+		tempf.write(line.replace(original, new))
+	foo_file.close()
+	tempf_path = tempf.name
+	tempf.close()
+	tempf = open(tempf_path, 'r')
+	os.unlink(path)
+	foo_file = open(path, 'w')
+	for line in tempf:
+		foo_file.write(line)
--- pros.py
+++ pros.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 import sys
 import os
@@ -7,6 +7,7 @@ import errno
 import urllib2
 import imp
 import zipfile
+import tempfile
 
 parser = argparse.ArgumentParser(description='Create and upgrade PROS projects.')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.5')
@@ -14,13 +15,15 @@ parser.add_argument('-k', '--kernel', type=str, nargs=1, metavar='V',
                     help='Kernel version to create or upgrade project to')
 parser.add_argument('directory', nargs='?', default='.',
                     help='Directory to create or upgrade. Defaults to current directory')
-parser.add_argument('-s', '--site', type=str, nargs=1, metavar='SITE',
-                    default='https://raw.githubusercontent.com/purduesigbots/purdueros-kernels/master/',
+parser.add_argument('-s', '--site', type=str, metavar='SITE',
+                    default='https://raw.githubusercontent.com/edjubuh/purdueros-kernels/master/',
                     help='Use a different site to pull kernels from.')
-parser.add_argument('-d', '--download', action='store_false',
+parser.add_argument('-d', '--download', action='store_true',
 		    help='Forces a redownload of the kernel\' template. Works even when --kernel is not provided.')
 parser.add_argument('-f', '--force', '--fresh', action='store_true', 
                     help='Deletes all contents in the directory and fills it with a new PROS template.')
+parser.add_argument('-n', '--name', '--project-name', type=str, nargs=1, metavar='NAME',
+                    help='Name of the project. Defaults to the name of the current directory')
 parser.add_argument('--verbose', action='store_true',
 		    help='Prints verbosely.')
 
@@ -34,6 +37,9 @@ if os.name == 'posix':
 elif os.name == 'nt':
 	kernelDirectory = 'C:\ProgramData\PROS\kernels'
 
+if args.verbose:
+	print('Kernel directory is ' + kernelDirectory)
+
 # create if not exists
 try:
 	os.makedirs(kernelDirectory)
@@ -43,48 +49,60 @@ except OSError as exception:
 	elif args.verbose:
 		print(kernelDirectory + ' already exists')
 
-# try to fetch the latest if args.kernel is undefined
+# try to fetch the latest kernel version online if args.kernel is undefined
 if not args.kernel:
+	if args.verbose:
+		print('Determining latest kernel from internet.')
 	try:
 		args.kernel = urllib2.urlopen(args.site + 'latest.kernel').read()
+		if args.verbose:
+			print('Latest kernel version is ' + args.kernel)
 	except urllib2.HTTPError as exception:
 		if args.verbose:
-			print('Unable to fetch kernel from ' + args.site)
+			print('Unable to fetch kernel from ' + args.site + 'latest.kernel')
 		pass
 
 # if we couldn't fetch the latest online, look for the highest (alphabetically) local kernel
 if not args.kernel:
 	if args.verbose:
-		print('Local kernels: ' + ' '.join(sorted([x[0][len(kernelDirectory):] for x in os.walk(kernelDirectory)], reverse=True)))
-	args.kernel = sorted([x[0][len(kernelDirectory):] for x  in os.walk(kernelDirectory)], reverse=True)[0]
+		print('All local kernels: ' + ' '.join(sorted([x[1] for x in os.walk(kernelDirectory)][0])))
+	args.kernel = sorted([x[1] for x in os.walk(kernelDirectory)][0])[0]
+
+args.kernel = args.kernel.strip()
+
+# give up if we still don't have a target kernel
+if not args.kernel:
+    sys.exit('Error: Could not determine a target kernel.')
 
 # determine if we have the downloaded kernel, if not, download it
-if args.kernel in [x[len(kernelDirectory):] for x in  [x[0] for x in os.walk(kernelDirectory)]]:
-	if args.verbose:
-		print(args.kernel + ' is available locally.')
-else:
+if not args.kernel in [x[1] for x in os.walk(kernelDirectory)][0] or args.download:
 	if args.verbose:
 		print(args.kernel + ' must be downloaded.')
 	downloadUrl = args.site + args.kernel + '.zip'
 	try:
 		from StringIO import StringIO
 		zipdata = StringIO()
-		zipdata.write(urllib2.urlopen(downloadUrl).read())
-		zipf = zipfile.ZipFile(zipdata)
-		zipf.extractall(kernelDirectory + '/' +  args.kernel + '/')
+		tempf = tempfile.NamedTemporaryFile(mode='w+b', suffix='.zip', delete=False)
+		tempf.write(urllib2.urlopen(downloadUrl).read())
+		tempf.close()
+		zipf = zipfile.ZipFile(tempf.name)
+		zipf.extractall(kernelDirectory + os.path.sep +  args.kernel + os.path.sep)
 	except urllib2.HTTPError as exception: # unable to download. give up
 		print('Unable to download requested kernel at ' + downloadUrl)
+elif args.kernel in [x[1] for x in os.walk(kernelDirectory)][0]:
+	if args.verbose:
+		print(args.kernel + ' is available locally.')
 
 # give up if we don't have a kernel or a locally avialable kernel yet
-if not args.kernel or not args.kernel in [x[0][len(kernelDirectory):] for x in os.walk(kernelDirectory)]:
-	sys.exit('Could not obtain kernel ' + args.kernel + '. Unable to download and not locally available.')
-
+if not args.kernel or not args.kernel in [x[0][len(kernelDirectory) + 1:] for x in os.walk(kernelDirectory)]:
+	sys.exit('Error: Could not obtain kernel ' + args.kernel + '. Unable to download and not locally available.')
 
 if args.verbose:
-	print('Kernel directory is: ' + kernelDirectory + '\n')
+	print('Kernel directory is: ' + kernelDirectory + os.path.sep + args.kernel + '\n')
+
 # try importing the upgrade module from the kernel directory if it's found. If not, use default one
 try:
-	kernelModule = 	kernelDirectory + args.kernel + '/' + args.kernel + '.py'
+	kernelModule = 	kernelDirectory + args.kernel + os.path.sep + args.kernel + '.py'
 	if args.verbose:
 		print('Attempting to load ' + kernelModule)
 	execute = imp.load_source(args.kernel, kernelModule)
@@ -100,16 +118,23 @@ if args.verbose:
 	print('Changing pathname. ' + args.directory + ' -> ' + os.path.abspath(args.directory))
 
 args.directory = os.path.abspath(args.directory)
+
+if not args.name:
+	args.name = args.directory.split(os.path.sep)[-1]
+
+if args.verbose:
+	print('Project name is: ' + args.name)
+
 if os.path.isdir(args.directory):
 	if args.force:
 		if args.verbose:
-			print('Overwriting directory in ' + args.directory + '\n')
-		execute.create(args.directory)
+			print('Overwriting project in ' + args.directory + '\n')
+		execute.create(args.directory, kernelDirectory + os.path.sep + args.kernel, args.name)
 	else:
 		if args.verbose:
-			print('Upgrading directory in ' + args.directory + '\n')
-		execute.upgrade(args.directory)
+			print('Upgrading project in ' + args.directory + '\n')
+		execute.upgrade(args.directory, kernelDirectory + os.path.sep +  args.kernel, args.name)
 else:
 	if args.verbose:
-		print('Creating directory in ' + args.directory + '\n')
-	execute.create(args.directory)
+		print('Creating project in ' + args.directory + '\n')
+	execute.create(args.directory, kernelDirectory + os.path.sep +  args.kernel, args.name)
